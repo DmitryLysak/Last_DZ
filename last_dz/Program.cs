a099e82d5a234f886ff7a36034a0a9978aa045db
Dmitry Lysak <lysak.d@gmail.com>
2023-03-21 20:14:27 +0300

    Добавили метод - проверка условия и присвоене значений для второго массива

--- last_dz/Program.cs
+++ last_dz/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array1 = new string[8] {"Hello", "2", "world", ":-)", "1234", "Russia", "-2", "computer science"};
 string[] array2 = new string[array1.Length];
+void Array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+Array(array1, array2);
